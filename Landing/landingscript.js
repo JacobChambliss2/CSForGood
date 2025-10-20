86406c22d04470c3f137478bbb5ac54a2a60299d
thedanphan <dphan4206@gmail.com>
2025-10-19 20:35:52 -0500

    added a typing effect rather than a scrolling animation thing

--- Landing/landingscript.js
+++ Landing/landingscript.js
@@ -8,3 +8,43 @@ function googleTranslateElementInit() {
         'google_translate_element'
     );
 }
+//source: https://stackoverflow.com/questions/54849362/how-to-achieve-this-typing-deleting-effect
+const words = ["Tutorly Needs", "Tutoring Questions", "Academic Help", "Engagement with Others"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            setTimeout(deletingEffect, 1000);
+            return false;
+        };
+        timer = setTimeout(loopTyping, 150);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 70);
+    };
+    loopDeleting();
+};
+
+typingEffect();
